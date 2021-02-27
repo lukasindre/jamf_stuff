18d9139f6d1dcaf045b6102ea1b564a8ecdc727a
Lukas <lukas.indre@upstart.com>
2021-02-27 07:32:36 -0500

    create choice between direct path call or end point call, makes easy for pagination purposes

--- get_apps.py
+++ get_apps.py
@@ -10,16 +10,16 @@ computer_apps = None
 if os.path.exists("apps.pickle"):
     computer_apps = pickle.load(open("apps.pickle", "rb"))
 else:
-    response = client.jamf_get("/computers")
+    response = client.jamf_get(True, "/computers")
     computer_ids = []
     for computer in response.json()['computers']:
         computer_ids.append(computer['id'])
 
     computer_apps = []
     for id in computer_ids:
-        response = client.jamf_get(f"/computers/id/{id}")
+        response = client.jamf_get(True, f"/computers/id/{id}")
         for app in response.json()['computer']['software']['applications']:
-            computer_apps.append(app['name'])
+            computer_apps.append(app['path'])
 
     pickle.dump(computer_apps, open("apps.pickle", "wb"))
 
